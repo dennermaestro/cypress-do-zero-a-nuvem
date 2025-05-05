5d2e5883f0bc593cafff391ed5f804daef6a5af6
Denner Santana <dennersantana@MacBook-Air-de-Denner.local>
2025-05-05 11:07:53 -0300

    pageload was added on git

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   viewportHeight: 880,
   viewportWidth: 1280,
+  pageLoadTimeout: 90000,
   e2e: {}
   //video: true 
    
--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -249,7 +249,7 @@ cy.contains('a', 'Política de Privacidade')
 
 })
 
-it('acessa a página da política de privacidade removendo o target e então clicando no link', () => {
+it.only('acessa a página da política de privacidade removendo o target e então clicando no link', () => {
   cy.contains('a', 'Política de Privacidade').invoke('removeAttr', 'target')
   .click()
 cy.contains('h1', 'CAC TAT - Política de Privacidade').should('be.visible')
