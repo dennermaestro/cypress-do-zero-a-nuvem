b7d988960c51a7a4c2392a17f3d460e6b995365b
Denner Santana <dennersantana@MacBook-Air-de-Denner.local>
2025-05-05 21:49:36 -0300

    Config cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -4,10 +4,9 @@ module.exports = defineConfig({
   viewportHeight: 880,
   viewportWidth: 1280,
   //pageLoadTimeout: 120000,
-  e2e: {}
+  projectId: "p5oc9o",
+  e2e: {},
   //video: true 
    
 
-    
-  
 })
